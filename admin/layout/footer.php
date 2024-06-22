39244234a769789a4edab2557e1928f0887b7d93
mrisqikhasani <mrisqikhasani@gmail.com>
2024-06-22 17:56:22 +0700

    templating halaman admin

---
+++ admin/layout/footer.php
@@ -0,0 +1,53 @@
+            <footer class="footer">
+                <div class="container-fluid d-flex justify-content-center">
+                    <div class="copyright">
+                        2024 Develop by Developer <i class="fa fa-heart heart text-danger"></i> us
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+
+        <!-- End Custom template -->
+    </div>
+    <!--   Core JS Files   -->
+    <script src="../assets/admin/js/core/jquery-3.7.1.min.js"></script>
+    <script src="../assets/admin/js/core/popper.min.js"></script>
+    <script src="../assets/admin/js/core/bootstrap.min.js"></script>
+
+    <!-- jQuery Scrollbar -->
+    <script src="../assets/admin/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+
+    <!-- Chart JS -->
+    <script src="../assets/admin/js/plugin/chart.js/chart.min.js"></script>
+
+    <!-- jQuery Sparkline -->
+    <script src="../assets/admin/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Chart Circle -->
+    <script src="../assets/admin/js/plugin/chart-circle/circles.min.js"></script>
+
+    <!-- Datatables -->
+    <script src="../assets/admin/js/plugin/datatables/datatables.min.js"></script>
+
+    <!-- Bootstrap Notify -->
+    <!-- <script src="../assets/admin/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script> -->
+
+    <!-- jQuery Vector Maps -->
+    <script src="../assets/admin/js/plugin/jsvectormap/jsvectormap.min.js"></script>
+    <script src="../assets/admin/js/plugin/jsvectormap/world.js"></script>
+
+    <!-- Sweet Alert -->
+    <script src="../assets/admin/js/plugin/sweetalert/sweetalert.min.js"></script>
+
+    <!-- Kaiadmin JS -->
+    <script src="../assets/admin/js/kaiadmin.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $("#katalog-datatables").DataTable({});
+        });
+    </script>
+</body>
+
+</html>
